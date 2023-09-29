7fe786597383f7ca036793cffd2f6a3c6f1621b0
yagikshrimal <146158730+yagikshrimal@users.noreply.github.com>
2023-09-28 23:12:17 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+x=2
+y=5.6
+z=("Pta nhi")
+print(x,y,z)
+print(type(z))
+sum=x+y
+mul=x*y
+print(sum,mul)
+
+
+
---
+++ task1.py
@@ -0,0 +1,6 @@
+num1=input("Enter number 1  ")
+#by default input is taken as string
+num2=input("Enter number 2  ")
+#changing data type to string to int type casting
+sum=int(num1)+int(num2)
+print(sum)
